45fb7d3e5c5dee0b9f7124bfbe7de60290adfd55
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-02-27 18:45:08 -0700

    Create Shuffle method

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -12,14 +12,52 @@ namespace TwentyOne
         {
             Deck deck = new Deck();
 
-            Console.WriteLine(deck.Cards.Count);
-
-            foreach (Card card in deck.Cards)
+            Deck shuffledDeck = Shuffle(deck);
+            Console.WriteLine(shuffledDeck.Cards.Count);
+            foreach (Card card in shuffledDeck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
 
             Console.ReadLine();
+        }
+
+        // Shuffles the deck of cards. | Page 154
+        public static Deck Shuffle(Deck deck)
+        {
+            //Jesse's shuffled list mehtod
+            List<Card> tempList = new List<Card>();
+            Random rand = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                int randomIndex = rand.Next(0, deck.Cards.Count);
+                tempList.Add(deck.Cards[randomIndex]);
+                deck.Cards.RemoveAt(randomIndex);
+            }
+
+            deck.Cards = tempList;
+            return deck;
+
+
+            //My Shuffle method
+            //Random rand = new Random();
+            //Deck shuffledDeck = new Deck();
+            //shuffledDeck.Cards.Clear();
+
+            //int cardsLeft = deck.Cards.Count;
+            //while (cardsLeft > 0)
+            //{
+            //    int indexOfCard = rand.Next(0, cardsLeft - 1);
+            //    Card card = deck.Cards[indexOfCard];
+
+            //    shuffledDeck.Cards.Add(card);
+            //    deck.Cards.Remove(card);
+
+            //    cardsLeft = deck.Cards.Count;
+            //}
+
+            //return shuffledDeck;
         }
     }
 }
